c9a10b531c46a38c66e19c30761e30149add6cd7
temidrayon-star <temidrayon@gmail.com>
2026-07-16 03:22:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ from telethon.tl.types import (
     InputReportReasonOther
 )
 
-API_ID = YOUR_API_ID 
-apihash = "YOUR_API_HASH "
+API_ID = 30696046 
+apihash = "1c9c23533a9a112fca7e52a68f76ccac"
 
 class ziddi:
     def __init__(self, api_id, api_hash):
